498879814b8bf7c5531ee52b86ef39db2cc117ff
Vale-riad <valentinariad91@gmail.com>
2023-02-15 11:10:37 +0100

    Aggiunge button

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    Hello World
-  </div>
+  <div>Hello World</div>
   <h1>Deliveboo</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quam suscipit asperiores doloribus quod, amet
-    dicta blanditiis minus! Consequuntur eveniet enim illum cumque. Sint nostrum facilis ea aliquid! Maxime, maiores?
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quam
+    suscipit asperiores doloribus quod, amet dicta blanditiis minus!
+    Consequuntur eveniet enim illum cumque. Sint nostrum facilis ea aliquid!
+    Maxime, maiores?
   </p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis nemo totam, voluptates delectus vel beatae
-    architecto nisi doloremque earum quasi placeat maxime, quis dolore id veniam et veritatis sed!</p>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis
+    nemo totam, voluptates delectus vel beatae architecto nisi doloremque earum
+    quasi placeat maxime, quis dolore id veniam et veritatis sed!
+  </p>
+  <button>Crea</button>
 </template>
 
 <script>
@@ -16,6 +21,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
