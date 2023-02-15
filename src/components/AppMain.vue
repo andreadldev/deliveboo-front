ca93e30303c5061e5bd740a1b3b518a14c2432cd
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-15 10:59:06 +0100

    New section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,10 @@
   <div>
     Hello World
   </div>
+  <h1>Deliveboo</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quam suscipit asperiores doloribus quod, amet
+    dicta blanditiis minus! Consequuntur eveniet enim illum cumque. Sint nostrum facilis ea aliquid! Maxime, maiores?
+  </p>
 </template>
 
 <script>
