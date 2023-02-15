f8b708fd5e5f2ab6ab0924bc68246315c260316b
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-15 11:54:09 +0100

    Elimina prova

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,24 +1,5 @@
 <template>
-  <div>Hello World</div>
-  <h1>Deliveboo</h1>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quam
-    suscipit asperiores doloribus quod, amet dicta blanditiis minus!
-    Consequuntur eveniet enim illum cumque. Sint nostrum facilis ea aliquid!
-    Maxime, maiores?
-  </p>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis
-    nemo totam, voluptates delectus vel beatae architecto nisi doloremque earum
-    quasi placeat maxime, quis dolore id veniam et veritatis sed!
-  </p>
-  <button>Crea</button>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-    tenetur fuga at, perspiciatis nam aspernatur voluptate, voluptates qui
-    deleniti optio iure officiis voluptas reprehenderit modi enim omnis quos
-    facilis veniam?
-  </p>
+
 </template>
 
 <script>
@@ -27,4 +8,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
