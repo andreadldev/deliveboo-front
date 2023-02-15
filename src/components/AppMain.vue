6d9d266d9004c4de4090fe8a7b2d54543960dc8e
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-15 11:05:46 +0100

    paragrafo

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,6 +6,8 @@
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quam suscipit asperiores doloribus quod, amet
     dicta blanditiis minus! Consequuntur eveniet enim illum cumque. Sint nostrum facilis ea aliquid! Maxime, maiores?
   </p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis nemo totam, voluptates delectus vel beatae
+    architecto nisi doloremque earum quasi placeat maxime, quis dolore id veniam et veritatis sed!</p>
 </template>
 
 <script>
