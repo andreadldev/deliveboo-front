5a6a032f9114aff70c95b797e647d6e3432763d3
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-22 17:59:30 +0100

    Aggiunge background header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,24 +5,7 @@ export default {
 </script>
 
 <template>
-  <!-- <header class="container ">
-
-    <div class="d-flex justify-content-between">
-      <div><img src="logo" alt="logo"></div>
-      <div class="d-flex my-nav">
-        <div class="my-link-header">
-          <router-link :to="{ name: 'homepage' }">Home</router-link>
-        </div>
-        <div class="my-link-header">
-          <router-link :to="{ name: 'restaurants' }">Ristoranti</router-link>
-        </div>
-                                    <div class="my-link-header">
-                                      <router-link :to="{ name: 'about-us' }">Chi siamo</router-link>
-                                    </div>
-                                  </div>
-                                </div>
-                              </header> -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-md navbar-dark bg-warning">
     <div class="container">
       <a class="navbar-brand d-md-none d-xs-block py-3" href="#">
         <img src="" height="40" alt="Company Logo">
@@ -35,17 +18,20 @@ export default {
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item">
-            <router-link class="text-decoration-none nav-link mx-2" :to="{ name: 'homepage' }">Home</router-link>
+            <router-link class="text-decoration-none text-success nav-link mx-2"
+              :to="{ name: 'homepage' }">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="text-decoration-none nav-link mx-2" :to="{ name: 'restaurants' }">Ristoranti</router-link>
+            <router-link class="text-decoration-none text-success nav-link mx-2"
+              :to="{ name: 'restaurants' }">Ristoranti</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="text-decoration-none nav-link mx-2" :to="{ name: 'about-us' }">Chi siamo</router-link>
+            <router-link class="text-decoration-none text-success nav-link mx-2" :to="{ name: 'about-us' }">Chi
+              siamo</router-link>
           </li>
 
           <li class="nav-item">
-            <router-link class="text-decoration-none nav-link mx-2 btn rounded-0 btn-danger"
+            <router-link class="text-decoration-none nav-link mx-2 btn rounded-0 btn-info"
               :to="{ name: 'checkout' }">Ordina</router-link>
           </li>
         </ul>
@@ -60,10 +46,14 @@ export default {
 
 
 <style lang="scss" scoped>
-.my-nav {
-  .my-link-header {
-    margin: 10px;
-    text-decoration: none;
+@media screen and (min-width:777px) {
+  .nav-link {
+    padding: 18px 0;
+    min-width: 120px;
+  }
+
+  .navbar {
+    padding: 0
   }
 }
 </style>
