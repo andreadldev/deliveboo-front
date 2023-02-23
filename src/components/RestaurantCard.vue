07b6a44568a3683bbb02c7e0c53bceb9b7c4e786
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-23 15:27:28 +0100

    Stampa data in restaurant

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -8,17 +8,19 @@ export default {
 </script>
 
 <template>
-  <div class="card m-4">
+  <div class="w-100 card m-4">
     <div class="card-body">
       <h5 class="card-title">{{ data.name }}</h5>
-      <p class="card-text">
-        {{ data.description }}
-      </p>
-      <router-link
-        :to="{ name: 'single-restaurant', params: { slug: data.slug } }"
-        class="btn btn-primary"
-        >Leggi di più</router-link
-      >
+      <img v-if="data.img" :src="data.img" class="card-img-top" :alt="data.name">
+      <small>{{ data.phone_number }}</small>
+      <span>{{ data.opening_time }}</span>
+      <span>{{ data.closing_time }}</span>
+      <div>
+        <span>€{{ data.price_shipping }}</span>
+      </div>
+
+      <router-link :to="{ name: 'single-restaurant', params: { slug: data.slug } }" class="btn btn-primary">Leggi di
+        più</router-link>
     </div>
   </div>
 </template>
