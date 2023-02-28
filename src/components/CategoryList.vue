bc3021662d23512410c20e679dc0f095dab46990
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-28 16:43:27 +0100

    Aggiunge hero

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -50,12 +50,12 @@ export default {
     <div class="d-flex flex-column">
       <div>
         <h2 class="mt-5">Scegli una o più Categoria:</h2>
-        <ul class="d-flex flex-wrap justify-content-between gy-3 col-lg-9 m-auto category ">
+        <ul class="d-flex justify-content-around gy-3 col-lg-9 m-auto category ">
           <li v-for="(category, index) in store.categories" :key="index">
             <!-- <div>
-                  <input type="checkbox" v-model="category.selected" @change="toggleCategory(index)">
-                      {{ category.name }}
-                    </div> -->
+                      <input type="checkbox" v-model="category.selected" @change="toggleCategory(index)">
+                          {{ category.name }}
+                        </div> -->
             <article class="feature1">
               <input type="checkbox" id="feature1" v-model="category.selected" @change="toggleCategory(index)" />
               <div>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,9 +9,10 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="my-container">
+
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="hero d-flex align-items-center section-bg">
+    <section id="hero" class="container hero d-flex align-items-center section-bg">
       <div class="container">
         <div class="row justify-content-around gy-5 my-hero">
           <div
@@ -31,10 +32,15 @@ export default {
         </div>
       </div>
     </section>
-    <!-- End Hero Section -->
-    <div>
-      <CategoryList class="text-center" />
-    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#fdbb2d" fill-opacity="1"
+      d="M0,128L40,144C80,160,160,192,240,197.3C320,203,400,181,480,154.7C560,128,640,96,720,85.3C800,75,880,85,960,112C1040,139,1120,181,1200,192C1280,203,1360,181,1400,170.7L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z">
+    </path>
+  </svg>
+  <!-- End Hero Section -->
+  <div>
+    <CategoryList class="text-center" />
   </div>
 </template>
 
@@ -42,6 +48,11 @@ export default {
 /*--------------------------------------------------------------
 # Hero Section
 --------------------------------------------------------------*/
+
+.my-container {
+  background-color: rgb(253, 187, 45);
+}
+
 .hero {
   width: 100%;
   background-size: cover;
@@ -138,5 +149,4 @@ export default {
   to {
     transform: rotate(359deg);
   }
-}
-</style>
+}</style>
