7cac9e4893dd095ce1e185c9984bc61aa8a4964d
Iunora93 <eleonoraruggieri22@gmail.com>
2023-03-01 17:26:44 +0100

    Aggiunge box shadow e hover

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -8,21 +8,10 @@ export default {
 </script>
 
 <template>
-  <div
-    class="w-100 h-100 card m-4 g-4 my-card-big"
-    style="background-color: rgb(253, 187, 45)"
-  >
-    <router-link
-      :to="{ name: 'single-restaurant', params: { slug: data.slug } }"
-      class="text-white"
-    >
+  <div id="demoRest" class="w-100 h-100 card m-4 g-4 my-card-big" style="background-color: rgb(253, 187, 45)">
+    <router-link :to="{ name: 'single-restaurant', params: { slug: data.slug } }" class="text-white">
       <div class="card-body p-0 d-flex flex-column justify-content-between">
-        <img
-          v-if="data.img"
-          :src="data.img"
-          class="card-img my-card"
-          :alt="data.name"
-        />
+        <img v-if="data.img" :src="data.img" class="card-img my-card" :alt="data.name" />
         <h5 class="card-title text-center">{{ data.name }}</h5>
 
         <div>
@@ -35,7 +24,7 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  border-color: rgba(197, 194, 189, 0.5);
+  border-color: rgba(216, 143, 84, 0.5);
   border-width: 3px;
 }
 
@@ -44,6 +33,17 @@ export default {
   width: 150px;
   height: 100px;
 }
+
+#demoRest {
+  -webkit-box-shadow: 0px 0px 25px 0px #ffc44a;
+  box-shadow: 0px 0px 25px 0px #ffc44a;
+}
+
+#demoRest:hover {
+  transform: scale(1.1);
+  /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+
 .my-card {
   height: 200px;
   object-fit: cover;
