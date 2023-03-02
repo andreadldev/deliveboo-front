2dd671468a046b77d79b7c4c0e0f82d986f82599
Vale-riad <valentinariad91@gmail.com>
2023-03-02 19:17:30 +0100

    Elimina Restaurantslist

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -3,8 +3,6 @@ import { store } from "../store";
 import CategoryCard from "./CategoryCard.vue";
 import RestaurantsList from "./RestaurantsList.vue";
 import RestaurantCard from "./RestaurantCard.vue";
-import RestaurantsList from "./RestaurantsList.vue";
-
 export default {
   name: "CategorysList",
   components: {
@@ -16,7 +14,6 @@ export default {
     return {
       store,
       visible: false,
-
       images: [
         "https://thumbs.dreamstime.com/b/memorial-day-usa-independence-picnic-party-fourth-july-concept-patriotic-american-traditional-food-watermelon-burgers-hot-219579658.jpg",
         "https://shfoodspro.com/wp-content/uploads/2021/02/Vietnamese-Pho.jpg",
@@ -38,7 +35,6 @@ export default {
       const selectedCategories = this.store.categories
         .filter((category) => category.selected)
         .map((category) => category.name);
-
       // filtro i risultati basandomi su quell'array
       return this.store.restaurants.filter((restaurant) => {
         // prendo l'array dei nomi delle categorie per quei ristoranti
@@ -63,115 +59,6 @@ export default {
 };
 </script>
 <template>
-<<<<<<< HEAD
-    <div class="my-category">
-        <div class="d-flex flex-column">
-            <h2 class="mt-5 ">Seleziona una o più Categorie:</h2>
-            <div class="m-auto">
-
-                <ul class="container d-flex flex-wrap justify-content-start">
-                    <li v-for="(category, index) in store.categories" :key="index">
-                        <!-- <div>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      <input type="checkbox" v-model="category.selected" @change="toggleCategory(index)">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          {{ category.name }}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </div> -->
-                        <article id="demo" class="feature1">
-                            <input type="checkbox" id="feature1" v-model="category.selected"
-                                @change="toggleCategory(index)" />
-
-                            <div class="w-200 d-flex flex-column justify-content-between">
-                            <img class="img-fluid w-200" :src="images[index]" alt="" />
-                            <span class="my-span"> {{ category.name }}<br /> </span>
-                        </div>
-
-                    </article>
-
-
-                </li>
-            </ul>
-
-
-            </div>
-            <!-- INIZIO CAROSELLO -->
-            <!-- <div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="false"> -->
-            <!-- bottoni slide centrali sotto -->
-            <!-- <div class="carousel-indicators">
-                                                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                                                        aria-current="true" aria-label="Slide 1"></button>
-                                                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                                                        aria-label="Slide 2"></button>
-
-                                                </div> -->
-            <!-- fine bottoni slide centrali sotto -->
-
-            <!-- intero carosello -->
-            <!-- <div class="carousel-inner"> -->
-            <!-- 1 pagina -->
-            <!-- <div class="carousel-item active d-flex">
-                                                        <div v-for="(category, index) in store.categories" :key="index" class=" card-group col-2 container">
-                                                            <div class="card ">
-                                                                <img src="..." class="card-img-top" alt="...">
-                                                                <div class="card-body">
-                                                                    <h5 class="card-title">{{ category.name }}</h5>
-
-                                                                </div>
-                                                            </div>
-
-
-                                                        </div>
-
-                                                    </div> -->
-            <!-- fine 1 pagina -->
-
-            <!-- 2 pagina -->
-            <!-- <div class="carousel-item">
-                                                        <div class="card-group"> -->
-
-
-            <!-- </div> -->
-
-            <!-- </div> -->
-            <!-- fine 2 pagina -->
-
-            <!-- 3 pagina -->
-
-            <!-- fine 3 pagina -->
-
-            <!-- </div> -->
-            <!-- fine intero carosello -->
-
-            <!-- bottoni slide -->
-            <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                                                    data-bs-slide="prev">
-                                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                                    <span class="visually-hidden">Previous</span>
-                                                </button>
-                                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                                                    data-bs-slide="next">
-                                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                                    <span class="visually-hidden">Next</span>
-                                                </button> -->
-            <!-- fine bottoni slide -->
-
-            <!-- </div> -->
-            <!-- FINE CAROSELLO -->
-
-            <div class="container">
-                <div class="d-flex">
-                    <ul class="row" v-if="isActive">
-                        <h2>La tua selezione: </h2>
-                        <li class="col-4 my-card" v-for="(restaurant, index) in filteredRestaurants" :key="index">
-                            <RestaurantCard :data="restaurant" />
-                            <router-link :to="{
-                                name: 'single-restaurant',
-                                params: { slug: restaurant.slug },
-                            }"></router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-=======
   <div class="">
     <div class="d-flex flex-column">
       <div class="m-auto">
@@ -218,7 +105,6 @@ export default {
             </li>
           </ul>
           <div v-else><RestaurantsList /></div>
->>>>>>> vale-branch
         </div>
       </div>
     </div>
@@ -227,30 +113,25 @@ export default {
 
 <style lang="scss" scoped>
 .my-category {
-    margin-top: -100px;
-    margin-bottom: -50px;
+  margin-top: -100px;
+  margin-bottom: -50px;
 }
-
 h2 {
   color: rgb(195, 34, 34);
 }
-
 ul {
   list-style: none;
 }
-
 body {
   text-align: center;
   color: #f5f5f5;
   background-image: linear-gradient(-90deg, #0c0c0d, #1a1a1a);
   font-family: "Roboto";
 }
-
 .app {
   max-width: 300px;
   margin: 0 auto;
 }
-
 article {
   position: relative;
   width: 140px;
@@ -258,10 +139,8 @@ article {
   margin: 20px;
   float: left;
   border: 2px solid rgb(253, 187, 45);
-
   box-sizing: border-box;
 }
-
 article div {
   width: 100%;
   height: 100%;
@@ -271,7 +150,6 @@ article div {
   line-height: 25px;
   transition: 0.5s ease;
 }
-
 article input {
   position: absolute;
   top: 0;
@@ -281,31 +159,25 @@ article input {
   opacity: 0;
   cursor: pointer;
 }
-
 #demo {
   -webkit-box-shadow: 0px 0px 25px 0px #ffc44a;
   box-shadow: 0px 0px 25px 0px #ffc44a;
 }
-
 #demo:hover {
   transform: scale(1.2);
   /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
 }
-
 input[type="checkbox"]:checked ~ div {
   background-color: rgb(253, 187, 45);
 }
-
 .category {
   color: rgb(195, 34, 34);
   font-size: 18px;
 }
-
 .container {
   margin-top: 50px;
   padding-top: 50px;
 }
-
 .my-card {
   margin-top: 25px;
   margin-bottom: 25px;
