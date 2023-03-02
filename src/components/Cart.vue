dd6f9849741add3a5f70da2a5e50b6c6d01f41a3
Vale-riad <valentinariad91@gmail.com>
2023-03-02 19:05:42 +0100

    modifiche

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -180,15 +180,6 @@ export default {
     },
   },
 };
-const toastTrigger = document.getElementById("liveToastBtn");
-const toastLiveExample = document.getElementById("liveToast");
-if (toastTrigger) {
-  toastTrigger.addEventListener("click", () => {
-    const toast = new bootstrap.Toast(toastLiveExample);
-
-    toast.show();
-  });
-}
 </script>
 
 <template>
