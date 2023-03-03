ce93769d932d3fc205c59a328640016178ea1109
Vale-riad <valentinariad91@gmail.com>
2023-03-03 10:44:08 +0100

    Modifica proce_shipping

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -331,7 +331,7 @@ export default {
                   class="list-group-item d-flex justify-content-between align-items-center px-0"
                 >
                   Shipping
-                  <span> {{ this.rest }}</span>
+                  <span> {{ this.rest.price_shipping }}</span>
                 </li>
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3"
@@ -344,7 +344,10 @@ export default {
                   </div>
                   <span>
                     <strong id="price"
-                      >{{ parseFloat(this.subtotal) + parseFloat(this.rest) }}
+                      >{{
+                        parseFloat(this.subtotal) +
+                        parseFloat(this.rest.price_shipping)
+                      }}
                     </strong>
                   </span>
                 </li>
